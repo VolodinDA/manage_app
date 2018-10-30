d5bd250ed72b0ad890941428123d08c8c486baf0
VolodinDA <dvnn@bk.ru>
2018-10-30 02:51:59 -0700

    Create the Session controller, add Session routes, create the signin actions for user, add new remember methods to the User model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/new'
+  resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+  root 'static_pages#home'
+  match '/signup', to: 'users#new', via: 'get'
+  match '/signin', to:  'sessions#new', via: 'get'
+  match '/signout', to: 'sessions#destroy', via: 'delete'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
