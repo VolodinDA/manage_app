53e77066ae59461d041256f5c718e6c297de7019
VolodinDA <dvnn@bk.ru>
2018-10-31 12:59:57 -0700

    Create a New User page, but not signin

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 
 	def full_title(page_title)
-		base_title = "Welcome to The N Enterprise"
+		base_title = "Welcome to the N Enterprise"
 		if page_title.empty?
 			base_title
 		else
