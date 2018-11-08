e91d1b437b2c5a0b331092782ff215ff7e3d9d20
VolodiDA <dvnn@bk.ru>
2018-11-08 16:09:44 +0300

    Try form for Speeches

--- app/controllers/speeches_controller.rb
+++ app/controllers/speeches_controller.rb
@@ -1,4 +1,6 @@
 class SpeechesController < ApplicationController
+before_action :signed_in_user
+
   def create
     @language=Language.find(params[:speech][:language_id])
     current_user.learn_language!(@language)
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,6 @@ class UsersController < ApplicationController
 	
 	def new
 		@user=User.new
-		@language=Language.new
 	end 
 	
    def create
