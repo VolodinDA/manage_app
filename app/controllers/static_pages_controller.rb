54d608c3795429f7cb04dcd15d71bb82adc8095c
VolodinDA <dvnn@bk.ru>
2018-11-26 07:52:58 -0800

    Put render_async everywhere where i only can in Home page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,16 +1,31 @@
 class StaticPagesController < ApplicationController
 
   def home
-
-    @languages=Language.all
-    @rewards=Reward.all
-    @errands=Errand.all
-    @abilities=Ability.all
   end
 
   def department_form
     @departments=Department.all
     render partial: 'departments/department_form'
+  end
+
+  def language_form
+    @languages=Language.all
+    render partial: 'languages/language_form'
+  end
+
+  def reward_form
+    @rewards=Reward.all
+    render partial: 'rewards/reward_form'
+  end
+
+  def errand_form
+    @errands=Errand.all
+    render partial: 'errands/errand_form'
+  end
+
+  def ability_form
+    @abilities=Ability.all
+    render partial: 'abilities/ability_form'
   end
 
 end
